73f7e6b83a971afe49f81321e62acaf7df7276f8
Robert Pope <r.pope@me.com>
2013-06-21 17:22:50 -0500

    got the isPrime funtion up

--- hw1/primeHundred.js
+++ hw1/primeHundred.js
@@ -1,35 +1,40 @@
+#!/usr/bin/env node
+var fs = require('fs');
+var outfile = "hello.txt";
+
 function isPrime(integer) {
 //is the number below 2?- then it is out of bounds for this function
-if (integer < 2){
-console.log("Out of bounds Error");
-break
-}
+    if (integer <= 2) {
+        console.log("Out of bounds Error");
+        return;
+    }
+    var root = Math.sqrt(integer);
+    console.log("The square root of Integer" + integer + "is" + root);
+    for (var i=2; i<root; i++)
+    {
+        var ceiling = Math.ceil(integer/i);
+        var floor = Math.floor(integer/i);
+        if (ceiling == floor){
+            console.log("Not a Prime Number")
+        }
+        console.log("I: " + i);
+        console.log("Ceiling: " + ceiling);
+        console.log("Floor: " + floor);
+    }
 //divide the integer by x - where x is all whole numbers =>  SQRT of integer
 //is it a whole number?
 //Yes - return FALSE - number is not prime
 //none of the numbers up to x returned a whole number when dividing integer by x?
 //Return TRUE - the number is an integer
-  return integer;
 }
+isPrime(15);
+//function getHundredPrimes() {
 
-function getHundredPrimes(){
-var i = 100;
-for j = 0, 100, j++{
-if j<i{
-  if (isPrime(i)){
-  primes[] = i;
-  if (primes.length = 100){
-    return primes;
-}
-}
-}
-}
+//}
 
-}
-
-var primes = array();
-primes = getHundredPrimes();
-primes = explode(primes, ',');
+//var primes = array();
+//primes = getHundredPrimes();
+//var out = explode(primes, ',');
 //print primes to a file
-
-console.log('file has been printed');
+//fs.writeFileSync(outfile, out);
+//console.log("Script: " + __filename + "\nWrote: 100 prime numbers To: " + outfile);
