9d1f9c087bbec6c5590eec748d7af78f8e0a1b5c
Robert Pope <r.pope@me.com>
2013-06-22 19:31:05 -0500

    one hundred primes assignment

--- hw1/primeHundred.js
+++ hw1/primeHundred.js
@@ -1,40 +1,40 @@
 #!/usr/bin/env node
 var fs = require('fs');
-var outfile = "hello.txt";
 
 function isPrime(integer) {
 //is the number below 2?- then it is out of bounds for this function
-    if (integer <= 2) {
+    if (integer <= 1) {
         console.log("Out of bounds Error");
         return;
     }
     var root = Math.sqrt(integer);
-    console.log("The square root of Integer" + integer + "is" + root);
-    for (var i=2; i<root; i++)
+    for (var i=2; i<=root; i++)
     {
         var ceiling = Math.ceil(integer/i);
         var floor = Math.floor(integer/i);
         if (ceiling == floor){
-            console.log("Not a Prime Number")
+            return false;
         }
-        console.log("I: " + i);
-        console.log("Ceiling: " + ceiling);
-        console.log("Floor: " + floor);
     }
-//divide the integer by x - where x is all whole numbers =>  SQRT of integer
-//is it a whole number?
-//Yes - return FALSE - number is not prime
-//none of the numbers up to x returned a whole number when dividing integer by x?
-//Return TRUE - the number is an integer
+    return true;
 }
-isPrime(15);
-//function getHundredPrimes() {
 
-//}
+function oneHundredPrimes(){
+    for(var i = 2, numOfPrimes = 0, primeNumbersArray = new Array();; i++){
+        if (isPrime(i)){
+            primeNumbersArray.push(i);
+            numOfPrimes = primeNumbersArray.length;
+            if (numOfPrimes >= 100){
+                break;
+            }
+        }
+    }
+    var output = primeNumbersArray.join();
+    console.log(numOfPrimes);
+    return output;
+}
 
-//var primes = array();
-//primes = getHundredPrimes();
-//var out = explode(primes, ',');
-//print primes to a file
-//fs.writeFileSync(outfile, out);
-//console.log("Script: " + __filename + "\nWrote: 100 prime numbers To: " + outfile);
+var out = oneHundredPrimes();
+var outfile = "oneHundredPrimes.csv";
+fs.writeFileSync(outfile, out);
+console.log("Script: " + __filename + "\nWrote: 100 prime numbers To: " + outfile);
